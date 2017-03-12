747323db944dca1cbf0ffe51d5bd9ad5b3ed605a
Snehal Shendware <snehalshendware1@gmail.com>
2017-03-11 21:51:57 -0700

    Alexa Skill "Baniya"
    A price finder for amazon marketplace.

--- index.js
+++ index.js
@@ -1,56 +1,125 @@
-var aws = require("aws-lib");
+exports.handler = (event, context, callback) => {
 
-var xml2js=require("xml2js");
+	var str = JSON.stringify(event);
 
-var accessKeyId='AKIAJFARVOS5FHAKPWAQ';
-
-var secretAccessKey='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';
-
-var associateTag='technocrat';
-
-var prodAdv = aws.createProdAdvClient(accessKeyId, secretAccessKey, associateTag);
-
-var options = {SearchIndex: "Electronics", Keywords: "Samsung Galaxy Tab 3"}
-
-var asinIds=[];
-
-prodAdv.call("ItemSearch", options, function(err, result) {
-    /*xml2js.parseString(result,function(err,jsonResult){
-        .log(jsonResult);
-    });*/
-    var items=result.Items.Item;
-
-    //console.log(items);
-
-    var len=items.length;
+    var request = event.request;
+    var intent = request.intent;
+    var slots = intent.slots;
     
-    for(var i=0;i<len;i++){
-    	asinIds.push(items[i].ASIN);
-    	console.log(asinIds[i]);
-    }
-    var len=asinIds.length;
-	options={};
-	options.ResponseGroup="Offers";
-	options.IdType="ASIN";
-	var asinToAbsolutePrice=[];
-	var asinToFormattedPrice=[];
+    var Categories = slots.Categories;
+    var Cvalue = Categories.value;
+    
+    var Product = slots.Product;
+    var Pvalue = Product.value;
 
-	for(var i=0;i<len;i++){
-	   options.ItemId=asinIds[i];
-	   var asin=asinIds[i];
-	   //console.log(JSON.stringify(options));
-	   prodAdv.call("ItemLookup", options, function(err,result){
-	   	  var lowest=result.Items.Item.OfferSummary.LowestNewPrice;
-	   	  if(lowest==undefined)
-	   	  	  return;
-	   	  asinToFormattedPrice[asin]=lowest.FormattedPrice;
-	   	  asinToAbsolutePrice[asin]=Number(lowest.Amount);
-	   	  console.log("ASIN:"+asin);
-	   	  console.log("Absolute Price:"+lowest.Amount);
-	   	  console.log("FormattedPrice:"+lowest.FormattedPrice);
+    var aws = require("aws-lib");
 
-	   });
+	var accessKeyId='';
+
+	var secretAccessKey='';
+
+	var associateTag='';
+
+	var prodAdv = aws.createProdAdvClient(accessKeyId, secretAccessKey, associateTag);
+
+	var options = {SearchIndex: "Electronics", Keywords: "Samsung Galaxy Tab 3"}
+
+	var asinIds=[];
+
+	if(Cvalue==undefined)
+		Cvalue="All";
+
+    console.log('Product Name : '+Pvalue + ' in Category '+ Cvalue);
+
+    var updatedCValue = Cvalue.charAt(0).toUpperCase() + Cvalue.slice(1);
+
+    console.log('Updated Category Name : '+updatedCValue);
+
+    options.Keywords = Pvalue;
+    options.SearchIndex = updatedCValue;
+
+    function sendMin(min) {
+					var response = {
+				        "version": "1.0",
+				        "response": {
+				            "outputSpeech": {
+				            "type": "PlainText",
+				            "text": "The best price for "+ Pvalue +" is "+min
+				        }
+					  }
+				  	}
+
+		console.log(JSON.stringify(response));  
+
+		callback(null, response);				 	
+
 	}
-});
+
+    prodAdv.call("ItemSearch", options, function(err, result) {
+
+
+    		console.log(JSON.stringify(result.Items));
+
+		    var items=result.Items.Item;
+
+		    var len=items.length;
+		    
+		    for(var i=0;i<3;i++){
+		    	asinIds.push(items[i].ASIN);
+		    	console.log(asinIds[i]);
+		    }
+		    var len=asinIds.length;
+			options={};
+			options.ResponseGroup="Offers";
+			options.IdType="ASIN";
+			var asinToAbsolutePrice=[];
+			var asinToFormattedPrice=[];
+
+			 var minObj={};
+
+			 minObj.min=Infinity;
+			 minObj.counter=asinIds.length; 
+
+			for(var i=0;i<asinIds.length;i++){
+			   options.ItemId=asinIds[i];
+			   var asin=asinIds[i];
+			   
+			   prodAdv.call("ItemLookup", options, function(err,result){
+
+			   	  var lowest=result.Items.Item.OfferSummary.LowestNewPrice;
+			   	  if(lowest==undefined){
+			   	  	  console.log("undefined lowest");
+			   	  	  return;
+			   	  }
+			   	  asinToFormattedPrice[asin]=lowest.FormattedPrice;
+			   	  var absolutePrice = Number(lowest.Amount);
+
+			   	  minObj.counter-=1;
+
+				  if(minObj.min > absolutePrice){
+				   	  	minObj.min = absolutePrice;
+				   	  	minObj.FormattedPrice=lowest.FormattedPrice;
+				  }
+				   
+				      //console.log("Minimum:"+minObj.min);	  
+				   	  asinToAbsolutePrice[asin]=Number(lowest.Amount);
+				   	  console.log("ASIN:"+asin);
+				   	  console.log("Absolute Price:"+lowest.Amount);
+				   	  console.log("FormattedPrice:"+lowest.FormattedPrice);
+
+				   	  if(minObj.counter==0){
+							sendMin(minObj.FormattedPrice);				   	  	
+				   	  }
+
+			   	  });
+
+			 } 
+			 	  	  
+	});
+
+
+
+};
+
 
 
